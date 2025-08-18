c78e82a42b6a3d7307c27cff6a31318f3fc0c12b
Manoel <candidomanoel78@gmail.com>
2025-08-18 12:03:22 -0300

    Envia questões 1003, 1004 e 1006

---
+++ Iniciante/1003.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    int soma;
+
+    scanf("%i", &a);
+    scanf("%i", &b);
+
+    soma = a + b;
+    
+    printf("SOMA = %i\n", soma);
+    return 0;
+}
---
+++ Iniciante/1004.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    int prod;
+
+    scanf("%i", &a);
+    scanf("%i", &b);
+
+    prod = a * b;
+    
+    printf("PROD = %i\n", prod);
+    return 0;
+}
