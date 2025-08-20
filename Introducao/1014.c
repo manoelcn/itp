8adecf19cf0d6074dca8d34ad30775ec1bf3c20e
Manoel <candidomanoel78@gmail.com>
2025-08-20 20:49:20 -0300

    Envia questão 1014

--- Introducao/1014.c
+++ Introducao/1014.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
 int main() {
+
     int distancia;
     double total_combustivel_gasto;
+    double resultado;
 
-    
+    scanf("%i", &distancia);
+    scanf("%lf", &total_combustivel_gasto);
+
+    resultado = distancia / total_combustivel_gasto;
+
+    printf("%.3f km/l\n", resultado);    
+
     return 0;
 }
