3a3e7152e5c538b7499a870ee8d4a92b469b6eee
Manoel <candidomanoel78@gmail.com>
2025-08-21 20:55:06 -0300

    Envia questão 1035

---
+++ Condicionais/1035.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b, c, d;
+    int somaAB, somaCD;
+
+    scanf("%i %i %i %i", &a, &b, &c, &d);
+
+    somaAB = a + b;
+    somaCD = c + d;
+
+    if (b > c && d > a && somaCD > somaAB && c > 0 && d > 0 && a % 2 == 0){
+        printf("Valores aceitos\n");
+    }
+    else{
+        printf("Valores nao aceitos\n");
+    }
+
+    return 0;
+}
