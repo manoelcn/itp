8046d52a9fee0f4e089a2cf8765d5a5f5b42cdfd
Manoel <candidomanoel78@gmail.com>
2025-08-24 00:23:42 -0300

    Envia questão 1045 completa

--- Condicionais/1045.c
+++ Condicionais/1045.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+    
     double a, b, c, A, B, C;
 
     scanf("%lf %lf %lf", &a, &b, &c);
@@ -36,7 +37,26 @@ int main() {
         }
     }
 
-    
+    if (A >= (B + C)){
+        printf("NAO FORMA TRIANGULO\n");
+    }
+    else{
+        if ((A * A) == (B * B) + (C * C)){
+        printf("TRIANGULO RETANGULO\n");
+        }
+        if ((A * A) > (B * B) + (C * C)){
+            printf("TRIANGULO OBTUSANGULO\n");
+        }
+        if ((A * A) < (B * B) + (C * C)){
+            printf("TRIANGULO ACUTANGULO\n");
+        }
+        if (A == B && A == C){
+            printf("TRIANGULO EQUILATERO\n");
+        }
+        if (A == B && C != A || B == C && A != B || A == C && A != B){
+            printf("TRIANGULO ISOSCELES\n");
+        }
+    }
 
     return 0;
 }
