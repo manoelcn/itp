e170c5b308ecd390144fbfac61dcddaab1d99d7b
Manoel <candidomanoel78@gmail.com>
2025-09-02 22:03:49 -0300

    Envia questão 1019

---
+++ Funcoes/1019.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int conversos_de_tempo(int segundos){
+    int hora, minuto, segundo, resto;
+
+    resto = segundos % 3600;
+    hora = segundos / 3600;
+    minuto = resto / 60;
+    segundo = resto % 60;
+
+    printf("%d:%d:%d\n", hora, minuto, segundo);
+}
+
+int main(){
+
+    int n;
+
+    scanf("%d", &n);
+
+    conversos_de_tempo(n);
+
+    return 0;
+}
