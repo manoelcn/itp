d06cbf0e4799a5a7ac39d2b76c4b442a067053c8
Manoel <candidomanoel78@gmail.com>
2025-10-21 11:23:01 -0300

    Envia questão 2108

--- Strings/2108.c
+++ Strings/2108.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
-    char maior_palavra[100];
-    while (1)
-    {
+int main(){
+    char maior_palavra[100] = "";
+    while (1){
         char string[100];
-        char *pedaco;
-        scanf("%s", string);
+        scanf(" %[^\n]", string);
 
-        if (strcmp(string, "0") == 0)
-        {
+        if (strcmp(string, "0") == 0){
             break;
         }
-        else
-        {
-            if ((int)strlen(string) >= (int)strlen(maior_palavra))
-            {
-                strcpy(maior_palavra, string);
+
+        char copia[100];
+        strcpy(copia, string);
+
+        char *pedaco = strtok(copia, " ");
+        if (pedaco != NULL){
+            printf("%d", (int)strlen(pedaco));
+
+            if ((int)strlen(pedaco) >= (int)strlen(maior_palavra)){
+                strcpy(maior_palavra, pedaco);
             }
 
-            pedaco = strtok(string, " ");
-            if (pedaco != NULL)
-            {
-                printf("%d", (int)strlen(pedaco));
-                pedaco = strtok(NULL, " ");
+            pedaco = strtok(NULL, " ");
+            while (pedaco != NULL){
+                printf("-%d", (int)strlen(pedaco));
 
-                while (pedaco != NULL)
-                {
-                    printf("-%d", (int)strlen(pedaco));
-                    pedaco = strtok(NULL, " ");
+                if ((int)strlen(pedaco) >= (int)strlen(maior_palavra)){
+                    strcpy(maior_palavra, pedaco);
                 }
+
+                pedaco = strtok(NULL, " ");
             }
         }
+        printf("\n");
     }
     printf("The biggest word: %s\n", maior_palavra);
 
